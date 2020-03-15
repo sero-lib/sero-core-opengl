dc765c0566218d200dcd6f4a250854cfba4d2385
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-03-15 01:37:42 +0100

    Docker now working, archlinux image

--- docker/compile_and_run.sh
+++ docker/compile_and_run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
+pacman --noconfirm -Syu
+
 cd /opt/repo || exit 1
 git pull
 git submodule foreach git pull
 
 mkdir build && cd build || exit 1
-cmake ..
-#    -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/clang++ \
-#    -DCMAKE_CC_COMPILER=/usr/lib/ccache/bin/clang \
-#    -DEXECUTABLE_OUTPUT_PATH=test.out
-
+cmake .. \
+    -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/clang++ \
+    -DCMAKE_CC_COMPILER=/usr/lib/ccache/bin/clang
 cmake --build .
 
-./test.out
+./sero_core_opengl_tests
